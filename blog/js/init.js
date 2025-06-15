1d3b9452ce7335a6fa2cb9e482a00688703a5de6
bambans <otavio@bambans.top>
2025-06-15 10:25:58 -0300

    update

--- blog/js/init.js
+++
@@ -1,5 +0,0 @@
-// Simple initialization for blog application
-'use strict';
-
-// BlogApp now handles its own initialization, so this file is minimal
-console.log('[Init] Blog initialization delegated to BlogApp');
